c8096fc989b16d64490b2ad0b1e4d000977c360a
Ori <OriBennett@gmail.com>
2024-01-16 15:32:40 +0200

    Connected to Mongo through Mongoose, added .env

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,28 @@
 import express from "express";
 import cors from "cors";
-const app = express();
+import mongoose from "mongoose";
+import User from "./models/User.js";
+import dotenv from "dotenv";
 
+const app = express();
+dotenv.config();
 app.use(cors()); // does nothing at the moment
 app.use(express.json()); // parses JSONs
 app.use(express.urlencoded({extended: false})); //this is common practice for urlencoded
 // these three lines are boilerplate
+const PORT = process.env.PORT || 8080; //make sure that you have a .env file
 
 //routes:
+app.post('/addUser', async(req, res) => {
+    // const user = req.body.user
+    const {user} = req.body;
+    const newUser = await User.create(user);
+    res.send(newUser);
+})
 
-app.listen(8080, function(){
-    console.log("listening on " + 8080);
-});
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+.then(() => {
+    app.listen(PORT, function(){
+        console.log("listening on " + PORT);
+    })
+}).catch(err => {console.log(err.message);});
