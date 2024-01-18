997db30d14939f8ae01f0670593e0bd0b0cd612e
Ori Bennett <oribennett@gmail.com>
2024-01-18 10:25:55 +0200

    added top margin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Title from './Components/Shared/Title.jsx'
+//import Title from './Components/Shared/Title.jsx'
 import Container from 'react-bootstrap/Container'
 import {HomePage} from './Pages/HomePage.jsx'
 import Footer from './Components/Shared/Footer'
@@ -14,7 +14,7 @@ function App() {
       <div className='d-flex flex-column side-allPage min-width'>
         {/* <Header/> */}
         <main>
-          <Container>
+          <Container className='mt-3'>
             <Routes>
               <Route path = "/" element = {<HomePage/>}></Route>
             </Routes>
