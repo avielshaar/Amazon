d7dbb527556e28abbd4501a7f8dfe6afac65d3d9
Ori Bennett <oribennett@gmail.com>
2024-01-18 11:31:46 +0200

    made the searcbox wider

--- frontend/src/Components/Shared/SearchBox.jsx
+++ frontend/src/Components/Shared/SearchBox.jsx
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button"
 
 const SearchBox = () => {
   return (
-    <Form className="d-flex me-auto">
+    <Form className="d-flex me-auto w-50">
         <InputGroup>
             <FormControl type="text" name="q" id="q" placeholder="Search for products" aria-describedby="button-search"></FormControl>
             <Button variant="outline-primary" id="button-search">
