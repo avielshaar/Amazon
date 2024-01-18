186f96cdcdae049b974fced63fa9fb099f999ce9
Ori Bennett <oribennett@gmail.com>
2024-01-18 11:13:39 +0200

    added font awsome and shopping cart icon

--- frontend/src/Components/Shared/Header.jsx
+++ frontend/src/Components/Shared/Header.jsx
@@ -1,6 +1,7 @@
 import NavBar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import {LinkContainer} from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
@@ -11,6 +12,12 @@ const Header = () => {
                         <img src="https://companieslogo.com/img/orig/AMZN_BIG.D-8fb0be81.png?t=1632523695" width={80} alt='Amazon logo'/>
                     </NavBar.Brand>
                 </LinkContainer>
+                {/* <SearchBox></SearchBox> */}
+                <nav className='d-flex align-items-center justify-content-end me-2 ms-4'>
+                    <Link to="/cart" className='nav-link'>
+                        <i className='fa fa-shopping-cart text-white'></i>
+                    </Link>
+                </nav>
             </Container>
         </NavBar>
     </header>   
