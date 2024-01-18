5b586de608f1f1d59bf7ab0559b6a6a06bb83142
Ori Bennett <oribennett@gmail.com>
2024-01-18 14:58:47 +0200

    get products to the homepage

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useEffect, useReducer } from 'react'
 import Title from '../Components/Shared/Title'
+import homePageReducer from '../Reducers/homePageReducer'
+import axios from 'axios'
 
+const initialState = {loading:true, error: "", data: []};
 export const HomePage = () => {
+  const [state, dispatch] = useReducer(homePageReducer, initialState);
+  useEffect(() => {
+    const getProducts = async () => {
+      dispatch({type:"GET_REQUEST"});
+      try {
+        const {data} = await axios.get("http://localhost:8080/api/v1/products")
+        dispatch({type:"GET_SUCCESS",payload: data});
+        console.log(data);
+      } catch (error) {
+        dispatch({type:"GET_FAIL",payload: error});
+        
+      }
+    };
+    getProducts();
+  },[])
+  
   return (
-    
     <div><Title title= "Home Page"/>
         HomePage</div>
   )
