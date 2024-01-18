49dc1add5bd96c5e6876cf5915d27d47456ad740
Ori Bennett <oribennett@gmail.com>
2024-01-18 11:24:24 +0200

    added searchbox

--- frontend/src/Components/Shared/Header.jsx
+++ frontend/src/Components/Shared/Header.jsx
@@ -2,6 +2,7 @@ import NavBar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import {LinkContainer} from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
+import SearchBox from './SearchBox';
 const Header = () => {
   return (
     <header>
@@ -12,12 +13,15 @@ const Header = () => {
                         <img src="https://companieslogo.com/img/orig/AMZN_BIG.D-8fb0be81.png?t=1632523695" width={80} alt='Amazon logo'/>
                     </NavBar.Brand>
                 </LinkContainer>
-                {/* <SearchBox></SearchBox> */}
+                <SearchBox/>
                 <nav className='d-flex align-items-center justify-content-end me-2 ms-4'>
                     <Link to="/cart" className='nav-link'>
                         <i className='fa fa-shopping-cart text-white'></i>
                     </Link>
                 </nav>
+                <Link to="/signin" className='text-white nav-link'>
+                    Sign-in
+                </Link>
             </Container>
         </NavBar>
     </header>   
