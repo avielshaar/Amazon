cf7a8d13a98524cf11020cdd2e84d5cc22aa893e
Ori Bennett <oribennett@gmail.com>
2024-01-23 10:22:27 +0200

    fixed wrong import

--- frontend/src/Pages/SignIn.jsx
+++ frontend/src/Pages/SignIn.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import axios from "axios"
 import Container from "react-bootstrap/Container"
 import Title from "../Components/Shared/Title"
-import { Form } from "react-router-dom"
+import Form from "react-bootstrap/Form"
 
 const SignIn = () => {
   const [email, setEmail] = useState("")
@@ -24,8 +24,12 @@ const SignIn = () => {
       <h1 className="my-3">Sign In</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3">
-          <Form.Label>Email</Form.Label>
+          <Form.Label>Email:</Form.Label>
           <Form.Control required onChange={(e) => setEmail(e.target.value)} placeholder="example@example.com"></Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" required onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password"></Form.Control>
         </Form.Group>
       </Form>
     </Container>
