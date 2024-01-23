25adeeb057289b046b69c6f0de066ca7c45f989c
Ori Bennett <oribennett@gmail.com>
2024-01-23 10:57:45 +0200

    signin completed successfully

--- frontend/src/Pages/SignIn.jsx
+++ frontend/src/Pages/SignIn.jsx
@@ -24,16 +24,16 @@ const SignIn = () => {
       <Title title="SignIn Page"/>
       <h1 className="my-3">Sign In</h1>
       <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email:</Form.Label>
           <Form.Control required onChange={(e) => setEmail(e.target.value)} placeholder="example@example.com"></Form.Control>
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password:</Form.Label>
           <Form.Control type="password" required onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password"></Form.Control>
         </Form.Group>
         <div className="mb-3">
-          <Button>Sign In</Button> 
+          <Button type="submit">Sign In</Button> 
         </div>
         <div className="mb-3">
           New customer? {" "} 
