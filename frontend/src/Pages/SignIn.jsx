6f311565825cf95d4adf57ba8724fadd25c26cd1
Ori Bennett <oribennett@gmail.com>
2024-01-23 09:42:44 +0200

    signin stuff

--- frontend/src/Pages/SignIn.jsx
+++ frontend/src/Pages/SignIn.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react"
+import axios from "axios"
 
 const SignIn = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.post("/api/v1/users/signin",{email: email, password: password});
+  }
   return (
     <div>SignIn</div>
   )
