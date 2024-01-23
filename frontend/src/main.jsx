16e5052590829aa9cf0f6243cce6773312dd3643
Ori Bennett <oribennett@gmail.com>
2024-01-23 12:18:04 +0200

    imported StoreProvider to main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
 import axios from "axios";
+import { StoreProvider } from "./Store.jsx";
 
 axios.defaults.baseURL = "http://localhost:8080"; // Change this to server once we have one in production or import from .env
 
