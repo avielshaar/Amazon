9dc07fa8f4159ef7609df9a02a838c0d57061886
Aviel Shaar <shaaraviel@gmail.com>
2024-03-27 10:24:54 +0200

    url adjustments

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { HelmetProvider } from "react-helmet-async";
-import axios from "axios";
-import { StoreProvider } from "./Store.jsx";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { HelmetProvider } from 'react-helmet-async';
+import axios from 'axios';
+import { StoreProvider } from './Store.jsx';
 
-axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://netflix-backend-nine.vercel.app';
+axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://amazon-backend-nine.vercel.app/';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StoreProvider>
       <HelmetProvider>
