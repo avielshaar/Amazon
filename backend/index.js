92b11213c682d84a290c489ee297519adbce2868
Aviel Shaar <shaaraviel@gmail.com>
2024-03-27 10:22:31 +0200

    Vercel adjustments

--- backend/index.js
+++ backend/index.js
@@ -7,29 +7,30 @@ import seedRouter from "./routes/seedRouter.js";
 import productsRouter from "./routes/productsRouter.js";
 import usersRouter from "./routes/usersRouter.js";
 
-const app = express();
 dotenv.config();
-app.use(cors()); // does nothing at the moment
-app.use(express.json()); // parses JSONs
-app.use(express.urlencoded({ extended: false })); //this is common practice for urlencoded
-// these three lines are boilerplate
-const PORT = process.env.PORT || 8080; //make sure that you have a .env file
+const app = express();
+const port = process.env.PORT || 8080;
+const connectionString = process.env.MONGO_CONNECTION_STRING;
 
-//routes:
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
 app.use("/api/v1/seed", seedRouter);
 app.use("/api/v1/products", productsRouter);
 app.use("/api/v1/users", usersRouter);
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING) //make sure that you have a .env file
-  .then(() => {
-    app.listen(PORT, function () {
-      console.log("listening on " + PORT);
-    });
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+export default app;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { HelmetProvider } from "react-helmet-async";
 import axios from "axios";
 import { StoreProvider } from "./Store.jsx";
 
-axios.defaults.baseURL = "http://localhost:8080"; // Change this to server once we have one in production or import from .env
+axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://netflix-backend-nine.vercel.app';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
